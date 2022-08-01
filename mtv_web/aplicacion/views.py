9ca10795498e8e7f508764cf82b32338abea7abb
Andres Suarez <santiagosuarez.99@hotmail.com>
2022-08-01 14:36:17 -0500

    Web Finalizada

--- mtv_web/aplicacion/views.py
+++ mtv_web/aplicacion/views.py
@@ -1,20 +1,21 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 from aplicacion.models import Familiares
 from django.template import Template, Context, loader
+from datetime import datetime
 
 # Create your views here.
 
 def index(request):
 
-    
 
     familiares = Familiares.objects.all()
 
-    familiares = {'familiares':familiares}
+    contexto = {'familiares':familiares,"datetime": datetime.now()}
 
     plantilla = loader.get_template("index.html")
 
-    documento = plantilla.render(familiares)
+    documento = plantilla.render(contexto)
 
     return HttpResponse(documento)
